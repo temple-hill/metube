a661ce131dfbe4affdd0374cc72329bb95fad284
teraoka <temple.hill@icloud.com>
2022-04-12 22:24:46 +0900

    list

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Layout from '../components/Layout/Layout';
 import { fetchPopularData } from '../apis/index';
+import { Store } from '../store/index';
+import VideoGrid from '../components/VideoGrid/VideoGrid';
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem';
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store)
+
   useEffect(() => {
-    console.log(process.env.REACT_APP_YOUTUBE_API_KEY)
     fetchPopularData().then((res) => {
       console.log('data', res)
+      setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
     })
   }, [])
 
   return (
     <Layout>
-      Top page
+      <VideoGrid>
+        {globalState.popular?.map((popular) => {
+          return (
+            <VideoGridItem
+              id={popular.id}
+              key={popular.id}
+              src={popular.snippet.thumbnails.medium.url}
+              title={popular.snippet.title}
+            />
+          )
+        })}
+      </VideoGrid>
     </Layout>
   )
 }
